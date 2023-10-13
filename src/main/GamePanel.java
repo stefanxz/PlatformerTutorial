fadf241ee14c9e8c564b0afd5af8ea7d568bb524
NavzarZar <razvan.craciun@cognitive.ro>
2023-10-13 22:12:07 +0200

    Created a map class and displayed it in the GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 public class GamePanel extends JPanel{
     //Coordinates where rectangle is drawn at
     private int xDelta = 100, yDelta = 100;
+    Map map = new Map();
     private int frames = 0;
     private long lastChecked = 0;
     //Constructor
@@ -16,7 +17,7 @@ public class GamePanel extends JPanel{
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
-        Map map = new Map(this);
+        map = new Map();
     }
     public void changeX(int value){
         this.xDelta += value;
@@ -39,9 +40,16 @@ public class GamePanel extends JPanel{
         //Keeping the drawing sheet clean
         super.paintComponent(g);
 
+        // Draw map
+        for(int i = 0; i < map.getMapList().size(); i++) {
+            for (int j = 0; j < map.getMapList().get(i); j++) {
+                g.fillRect(i * map.getMapElementWidth(), j * map.getMapElementHeight() , map.getMapElementWidth(), map.getMapElementHeight());
+            }
+        }
+
         //Draw functions
         g.fillRect(xDelta,yDelta,50, 50);
-        g.drawRect(400, 400, 2000, 1000);w
+        g.drawRect(400, 400, 2000, 1000);
         frames++;
         //Check if a second passes/ has passed.
         if(System.currentTimeMillis() - lastChecked >= 1000) {
@@ -49,5 +57,9 @@ public class GamePanel extends JPanel{
             System.out.println("FPS: " + frames);
             frames = 0;
         }
+    }
+
+    public void displayMap() {
+
     }
 }
--- src/main/Map.java
+++ src/main/Map.java
@@ -2,13 +2,34 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Map extends JPanel {
     private Graphics graphics;
-    public Map(GamePanel gamePanel)
+    private ArrayList<Integer> mapList = new ArrayList<>();
+
+    private int mapElementWidth = 50;
+    private int mapElementHeight = 100;
+
+    public Map()
     {
-//        graphics = new Graphics
-//       gamePanel.paintComponent(graphics);
+        mapList.add(1);
+        mapList.add(3);
+        mapList.add(1);
+        mapList.add(3);
+        mapList.add(1);
+        mapList.add(3);
     }
 
+    public int getMapElementWidth() {
+        return mapElementWidth;
+    }
+
+    public ArrayList<Integer> getMapList() {
+        return new ArrayList<>(mapList);
+    }
+
+    public int getMapElementHeight() {
+        return mapElementHeight;
+    }
 }
